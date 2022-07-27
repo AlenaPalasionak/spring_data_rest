b975af460b860ff39b9bd39f3d2b23e41748ca82
Lena Palasionak <19lenka87@tut.by>
2022-07-27 17:45:15 +0300

    Spring boot actuator

---
+++ src/main/java/com/lenapalasionak/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.lenapalasionak.spring.springboot.spring_data_rest.dao;
+
+
+import com.lenapalasionak.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {// в дженериках:
+
+}
